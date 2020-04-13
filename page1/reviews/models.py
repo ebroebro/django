6fd8525ea96dcb46a109eb7a9fb97d3db0adaa53
조제형 <jojehyung@naver.com>
2020-04-13 12:58:38 +0000

    model

--- page1/reviews/models.py
+++ page1/reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Reviews(models.Model):
+    title=models.CharField(max_length=100)
+    restaurant=models.CharField(max_length=100)
+    rank=models.IntegerField()
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
