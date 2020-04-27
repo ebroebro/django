9729a70b3e342c4fe58cff813b617ea692078535
조제형 <jojehyung@naver.com>
2020-04-27 04:18:50 +0000

    try

--- page1/reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Reviews
-
-
-# Register your models here.
-class ReviewAdmin(admin.ModelAdmin):
-    list_display=('title','restaurant','rank','content','created_at','updated_at')
-
-admin.site.register(Reviews)
--- page1/reviews/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Reviews
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model=Reviews
-        fields='__all__'
